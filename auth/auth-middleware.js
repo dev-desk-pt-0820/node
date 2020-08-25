61dd2ae8f34710fd04523e75f849403b27c9b7db
Nathan Brown <scottynate05@yahoo.com>
2020-08-24 22:22:18 -0700

    middleware

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,51 +1,51 @@
-const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets.js');
+// const jwt = require('jsonwebtoken');
+// const secrets = require('../config/secrets.js');
 
-module.exports = (req, res, next) => {
-    const [authType, token] = req.headers.authorization.split(' ');
-    console.log('token: ', token);
-
-    if (token) {
-        jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-            if (err) {
-                res.status(401).json({
-                    you: 'can\'t touch this (without a token).'
-                })
-            } else {
-                req.decodedJwt = decodedToken;
-                next();
-            }
-        })
-    } else {
-        res.status(401).json({
-            you: 'have no power here (without a token).'
-        })
-    }
-}
-
-// const jwt = require("jsonwebtoken");
-// const secrets = require("../config/secrets.js");
 // module.exports = (req, res, next) => {
-//   try {
-//     const [authType, token] = req.headers.authorization.split(" ");
-//   } catch {
-//     token = "";
-//   }
-//   console.log("token: ", token);
-//   if (token) {
-//     jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-//       if (err) {
+//     const [authType, token] = req.headers.authorization.split(' ');
+//     console.log('token: ', token);
+
+//     if (token) {
+//         jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+//             if (err) {
+//                 res.status(401).json({
+//                     you: 'can\'t touch this (without a token).'
+//                 })
+//             } else {
+//                 req.decodedJwt = decodedToken;
+//                 next();
+//             }
+//         })
+//     } else {
 //         res.status(401).json({
-//           you: "can't touch this (without a token).",
-//         });
-//       } else {
-//         req.decodedJwt = decodedToken;
-//         next();
-//       }
-//     });
-//   } else {
-//     res.status(401).json({
-//       you: "have no power here (without a token).",
-//     });
-//   }
-// };
+//             you: 'have no power here (without a token).'
+//         })
+//     }
+// }
+
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
+module.exports = (req, res, next) => {
+  try {
+    const [authType, token] = req.headers.authorization.split(" ");
+  } catch {
+    token = "";
+  }
+  console.log("token: ", token);
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({
+          you: "can't touch this (without a token).",
+        });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({
+      you: "have no power here (without a token).",
+    });
+  }
+};
